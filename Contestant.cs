eccd3b7994e40365ab3705e6dad4b94ce923fd16
Michael Rackowski <MichaelRackowski@gmail.com>
2019-09-18 14:21:29 -0500

    Worked on stack and queue manager classes

---
+++ Contestant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    public class Contestant
+    {
+        public string firstname;
+        public string lastname;
+        public string email;
+        public int registration;
+
+         public Contestant(string firstname, string lastname,string email,int registration)
+        {
+            Contestant contestant = new Contestant(firstname,lastname,email,registration);
+           this.firstname = firstname;
+           this.lastname = lastname;
+           this.email = email;
+           this.registration = registration;
+        }
+    }
+}
---
+++ UserInterface.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+  public static class UserInterface
+    {
+       public void ContestantCreator(Contestant contestant)
+        {
+            Console.WriteLine("What is your first name?");
+           contestant.firstname = Console.ReadLine().ToLower();
+
+            Console.WriteLine("What is your last name?");
+            contestant.lastname = Console.ReadLine().ToLower();
+
+            Console.WriteLine("What is your email?");
+            contestant.email = Console.ReadLine().ToLower();
+
+            Console.WriteLine("What is your registration number?");
+            contestant.registration = Convert.ToInt32(Console.ReadLine().ToLower());
+
+        }
+    }
+}
